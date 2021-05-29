1df21fece0a767af3fc4b795ad74fad84ccac3de
1NQ457 <alex.kot14@gmail.com>
2021-05-29 16:29:52 +0300

    tests added

--- test/ipgeobase_test.rb
+++ test/ipgeobase_test.rb
@@ -1,13 +1,42 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class IpgeobaseTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Ipgeobase::VERSION
+  def setup # rubocop:disable Metrics/MethodLength
+    stub_request(:get, 'http://ip-api.com/xml/83.169.216.199')
+      .to_return(body: File.read('test/fixtures/succes.xml'))
+
+    stub_request(:get, 'http://ip-api.com/xml/8.8.8.8.8')
+      .to_return(body: File.read('test/fixtures/fail.xml'))
+
+    @correct_ip = '83.169.216.199'
+    @incorrect_ip = '8.8.8.8.8'
+
+    @sample_object = {
+      country: 'Russia',
+      countryCode: 'RU',
+      city: 'Yekaterinburg',
+      lat: 56.8333,
+      lon: 60.6
+    }
   end
 
-  def test_it_does_something_useful
-    assert false
+  def teardown; end
+
+  def test_correct_case
+    actual = Ipgeobase.lookup(@correct_ip)
+
+    assert_equal @sample_object[:country], actual.country
+    assert_equal @sample_object[:countryCode], actual.countryCode
+    assert_equal @sample_object[:city], actual.city
+    assert_equal @sample_object[:lat], actual.lat
+    assert_equal @sample_object[:lon], actual.lon
+  end
+
+  def test_incorrect_case
+    assert_raises RuntimeError do
+      Ipgeobase.lookup(@incorrect_ip)
+    end
   end
 end
